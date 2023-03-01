7966ab1e74abc33e88bba02b0b9063e09ef562ee
zainalam2 <zainalam2@yahoo.com>
2023-03-01 13:42:23 -0500

    final

--- js/main.js
+++ js/main.js
@@ -3,218 +3,184 @@
 
 
 // create frame constants
+const FRAME_WIDTH = 500;
 const FRAME_HEIGHT = 500;
-const FRAME_WIDTH = 500; 
-const MARGINS = {left: 50, right: 50, top: 50, bottom: 50};
-const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
-const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right; 
+const MARGINS = {left:50, right:50, top:50, bottom:50};
+const VIS_HEIGHT = FRAME_HEIGHT - (MARGINS.top + MARGINS.bottom);
+const VIS_WIDTH = FRAME_WIDTH - (MARGINS.left + MARGINS.right);
+
+// create svg for the 3 plots
+const FRAME1 = d3.select("#plot1")
+.append("svg")
+.attr("height", FRAME_HEIGHT)
+.attr("width", FRAME_WIDTH)
+.attr("class", "frame");
 
 
-// create frame for scatter plot
-const FRAME1 = d3.select("#plot1") 
-                  .append("svg") 
-                    .attr("height", FRAME_HEIGHT)   
-                    .attr("width", FRAME_WIDTH)
-                    .attr("class", "frame"); 
+const FRAME2 = d3.select("#plot2")
+.append("svg")
+.attr("height", FRAME_HEIGHT)
+.attr("width", FRAME_WIDTH)
+.attr("class", "frame");     
 
 
-// read data for scatter plot
-d3.csv("data/scatter-data.csv").then((data) => { 
+const FRAME3 = d3.select("#plot3")
+.append("svg")
+.attr("height", FRAME_HEIGHT)
+.attr("width", FRAME_WIDTH)
+.attr("class", "frame");  
+
+// read data from iris.csv
+d3.csv("data/iris.csv").then((DATA) => {
+
+    // add the color based on the species and specify the hex of each color
+  const SPECIES_COLOR = d3.scaleOrdinal()
+    .domain(["setosa", "versicolor", "virginica"])
+    .range(["red", "purple", "green"]);
+
+   
+  const MAX_X_PLOT_1 = d3.max(DATA, (d) => {
+    return parseInt(d.Sepal_Length);
+  });
+
+  
+    const MAX_Y_PLOT_1 = d3.max(DATA, (d) => {
+    return parseInt(d.Petal_Length);
+  }); 
+
+  
+  const X_SCALE_PLOT_1 = d3.scaleLinear()
+                    .domain([0, (MAX_X_PLOT_1 + 1)])
+                    .range([0, VIS_WIDTH]);
+
+ 
+  const Y_SCALE_PLOT_1 = d3.scaleLinear()
+          .domain([0, (MAX_Y_PLOT_1 + 1)])
+          .range([VIS_HEIGHT, 0]);
+    
+    
+    const MAX_X_PLOT_2 = d3.max(DATA, (d) => {
+    return parseInt(d.Sepal_Width);
+  });
 
     
-    // find max of x and y values
-    const MAX_X = d3.max(data, (d) => { return parseInt(d.x); });
-    const MAX_Y = d3.max(data, (d) => { return parseInt(d.y); });
+    const MAX_Y_PLOT_2 = d3.max(DATA, (d) => {
+    return parseInt(d.Petal_Width);
+  }); 
 
-    // create scales to map x and y values to pixels 
-    const X_SCALE = d3.scaleLinear() 
-                      .domain([0, (MAX_X + 1)]) 
-                      .range([0, VIS_WIDTH]); 
-    const Y_SCALE = d3.scaleLinear() 
-                      .domain([0, (MAX_Y + 1)]) 
-                      .range([VIS_HEIGHT, 0]); 
+    
+  const X_SCALE_PLOT_2 = d3.scaleLinear()
+                    .domain([0, (MAX_X_PLOT_2 + 1)])
+                    .range([0, VIS_WIDTH]);
 
+    
+  const Y_SCALE_PLOT_2 = d3.scaleLinear()
+          .domain([0, (MAX_Y_PLOT_2 + 1)])
+          .range([VIS_HEIGHT, 0]);
+    
+    // 50 species
+  const MAX_Y_BAR = 50;
 
-    // append svg circle points based on data
-  FRAME1.selectAll("point")  
-      .data(data) 
-      .enter()       
-      .append("circle")  
-        .attr("cx", (d) => { return (X_SCALE(d.x) + MARGINS.left); }) 
-        .attr("cy", (d) => { return (Y_SCALE(d.y) + MARGINS.bottom); })
-        .attr("r", 8)
+    
+    const X_SCALE_BAR = d3.scaleBand()
+    .domain(DATA.map((d) => { return d.Species; }))
+    .range([0, VIS_WIDTH])
+    .padding(0.2);
+    
+    
+    const Y_SCALE_BAR = d3.scaleLinear()
+    .domain([0, (MAX_Y_BAR)])
+    .range([VIS_HEIGHT, 0]);
+
+    // append the x and y axis to plot1
+  FRAME1.append("g")
+    .attr("transform", "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
+    .call(d3.axisBottom(X_SCALE_PLOT_1).ticks(10))
+        .attr("font-size", "15px");
+  FRAME1.append("g")
+    .attr("transform", "translate(" + MARGINS.left + "," + (MARGINS.bottom) + ")")
+    .call(d3.axisLeft(Y_SCALE_PLOT_1).ticks(10))
+        .attr("font-size", "15px");
+
+  // append x and y axis to plot2
+  FRAME2.append("g")
+    .attr("transform", "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
+    .call(d3.axisBottom(X_SCALE_PLOT_2).ticks(10))
+        .attr("font-size", "15px");
+  FRAME2.append("g")
+    .attr("transform", "translate(" + MARGINS.left + "," + (MARGINS.bottom) + ")")
+    .call(d3.axisLeft(Y_SCALE_PLOT_2).ticks(10))
+        .attr("font-size", "15px"); 
+
+  // append x and y axis to plot3
+  FRAME3.append("g")
+    .attr("transform", "translate(" + MARGINS.left + "," + (VIS_HEIGHT + MARGINS.top) + ")")
+    .call(d3.axisBottom(X_SCALE_BAR).ticks(10))
+    .attr("font-size", "15px");
+    FRAME3.append("g")
+    .attr("transform", "translate(" + MARGINS.left + "," + (MARGINS.bottom) + ")")
+    .call(d3.axisLeft(Y_SCALE_BAR).ticks(10))
+    .attr("font-size", "15px");
+
+  // add points to plot1
+  let GRAPH1 = FRAME1.selectAll("dot")
+        .data(DATA)
+        .enter()
+        .append("circle")
+        .attr("cx", (d) => { return X_SCALE_PLOT_1(d.Sepal_Length) + MARGINS.left; })
+    .attr("cy", (d) => { return Y_SCALE_PLOT_1(d.Petal_Length) + MARGINS.top; })
+        .attr("fill", (d) => { return SPECIES_COLOR(d.Species); })
+        .style("opacity", 0.5)
+        .attr("r", 5)
         .attr("class", "point");
 
+  // add points to plot2
+  let GRAPH2 = FRAME2.selectAll("dot")
+    .data(DATA)
+    .enter()
+    .append("circle")
+    .attr("cx", (d) => { return X_SCALE_PLOT_2(d.Sepal_Width) + MARGINS.left; })
+    .attr("cy", (d) => { return Y_SCALE_PLOT_2(d.Petal_Width) + MARGINS.top; })
+    .attr("fill", (d) => { return SPECIES_COLOR(d.Species); })
+    .style("opacity", 0.5)
+    .attr("r", 5)
+    .attr("class", "point");
 
-    // function to change color when mouse is hovering
-    function changeColor(){
-        d3.select(this)
-              .style("fill", "blue");
-    };
-
-
-    // function to return to normal color after mouse is not hovering
-    function normalColor(){
-        d3.select(this)
-              .style("fill", "darkred");
-    };
-        
-
-
-    const POINT_CLICK = d3.select("#information")
-                            .append("div");
-
-    function clickPoint(d){
-      const circle = d3.select(this);
-      circle.classed("bordered", !circle.classed("bordered"));
-      const xPt = X_SCALE.invert(circle.attr("cx"));
-      const yPt = Y_SCALE.invert(circle.attr("cy"));
-      POINT_CLICK.html("<br> Coordinates: (" + (Math.round(xPt) - 1) + "," + (Math.round(yPt) + 1) + ")");
-    };
+    // add brushing to plot2
+    FRAME2.call(d3.brush()                 
+    .extent([[0,0], [FRAME_WIDTH, FRAME_HEIGHT]]) 
+    .on("start brush", displayBrush)); 
 
 
-    // Add event listeners
-    FRAME1.selectAll(".point")
-          .on("mouseover", changeColor) 
-          .on("click", clickPoint)
-          .on("mouseleave", normalColor);    
 
+    // add data to plot3 (bar chart)
+    let GRAPH3 = FRAME3.selectAll("bars")
+    .data(DATA)
+    .enter()
+    .append("rect")
+        .attr("x", (d) => { return X_SCALE_BAR(d.Species) + MARGINS.left; })
+        .attr("y", (d) => { return Y_SCALE_BAR(MAX_Y_BAR) + MARGINS.top; })
+        .attr("width", X_SCALE_BAR.bandwidth())
+        .attr("height", (d) => { return VIS_HEIGHT - Y_SCALE_BAR(MAX_Y_BAR); })
+        .attr("fill", (d) => { return SPECIES_COLOR(d.Species); })
+        .style("opacity", 0.5)
+        .attr("class", "bar");
 
-    // append x axis to frame
-    FRAME1.append("g") 
-          .attr("transform", "translate(" + MARGINS.left + 
-                "," + (VIS_HEIGHT + MARGINS.top) + ")") 
-          .call(d3.axisBottom(X_SCALE).ticks(10)) 
-           
-
-    // append y axis to frame
-    FRAME1.append("g") 
-          .attr("transform", "translate(" + (MARGINS.left) + 
-                "," + (MARGINS.top) + ")") 
-          .call(d3.axisLeft(Y_SCALE).ticks(10)) 
-           
-
-
-    function addPoint(){
-        const xcoord = d3.select("#x-coord").property("value");
-        const ycoord = d3.select("#y-coord").property("value");
-
-        FRAME1.append("circle")
-                .attr("cx", (X_SCALE(xcoord) + MARGINS.left)) 
-                .attr("cy", (Y_SCALE(ycoord) + MARGINS.bottom))
-                .attr("r", 8)
-                .attr("class", "point")
-                .on("mouseover", changeColor) 
-                .on("click", clickPoint)
-                .on("mouseleave", normalColor);  
-    };
-
-    // create button
-    d3.select("#create-pt")
-    .on("click", addPoint); 
+    // shows the brushing
+    function displayBrush(event) {
+        selection = event.selection;
+        GRAPH1.classed("selected", function(d){ return isSelected(selection, X_SCALE_PLOT_2(d.Sepal_Width) + MARGINS.left, Y_SCALE_PLOT_2(d.Petal_Width) + MARGINS.top); })
+        GRAPH2.classed("selected", function(d){ return isSelected(selection, X_SCALE_PLOT_2(d.Sepal_Width) + MARGINS.left, Y_SCALE_PLOT_2(d.Petal_Width) + MARGINS.top); })
+        GRAPH3.classed("selected", function(d){ return isSelected(selection, X_SCALE_PLOT_2(d.Sepal_Width) + MARGINS.left, Y_SCALE_PLOT_2(d.Petal_Width) + MARGINS.top); })
+      };
+    
+      // when selecting a point to be brushed
+      function isSelected(coords, cx, cy) {
+        let x0 = coords[0][0],
+            x1 = coords[1][0],
+            y0 = coords[0][1],
+            y1 = coords[1][1];
+        return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1;
+      };
 
 });
-
-
-
-
-// bar plot frame
-const FRAME2= d3.select("#plot2") 
-                  .append("svg") 
-                    .attr("height", FRAME_HEIGHT)   
-                    .attr("width", FRAME_WIDTH)
-                    .attr("class", "frame"); 
-
-
-
-// read in bar data
-  d3.csv("data/bar-data.csv").then((data) => { 
-
-    
-    // create y axis scale           
-    const Y_SCALE = d3.scaleLinear() 
-                          .domain([100, 0]) 
-                          .range([0, VIS_HEIGHT]); 
-
-    // create x axis scale 
-    const X_SCALE = d3.scaleBand() 
-                               .domain(data.map((d) => { return d.category; })) 
-                               .range([0, VIS_WIDTH])
-                               .padding(.2); 
-
-
-    // plot bar based on data with rectangle svgs 
-    FRAME2.selectAll("bar")  
-          .data(data) 
-          .enter()       
-          .append("rect")  
-            .attr("y", (d) => { return Y_SCALE(d.amount) + MARGINS.bottom; }) 
-            .attr("x", (d) => { return X_SCALE(d.category) + MARGINS.left;}) 
-            .attr("height", (d) => { return VIS_HEIGHT - Y_SCALE(d.amount); })
-            .attr("width", 40)
-            .attr("class", "bar");
-
-
-
-       // append x axis to frame
-       FRAME2.append("g") 
-          .attr("transform", "translate(" + MARGINS.left + 
-                "," + (VIS_HEIGHT + MARGINS.top) + ")") 
-          .call(d3.axisBottom(X_SCALE))
-          
-
-
-    // append y axis to frame
-      FRAME2.append("g") 
-            .attr("transform", "translate(" + (MARGINS.left) + 
-                  "," + (MARGINS.top) + ")") 
-            .call(d3.axisLeft(Y_SCALE).ticks(10)) 
-            
-
-
-    // adding a tooltip
-    const TOOLTIP = d3.select("#plot2")
-                          .append("div")
-                          .attr("class", "tooltip")
-                          .style("opacity", 0); 
-
-
-    
-    function handleMouseover(event, d) {
-      // change color of bar when mouse is over it
-      d3.select(this)
-                .style("fill", "orange");
-     
-      TOOLTIP.style("opacity", 1);
-
-
-    };
-
-    function handleMousemove(event, d) {
-
-      // position the tooltip and fill in information 
-      TOOLTIP.html("Category: " + d.category + "<br>Amount: " + d.amount)
-              .style("left", (event.pageX + 10) + "px") 
-              .style("top", (event.pageY - 50) + "px"); 
-       
-    };
-
-    function handleMouseleave(event, d) {
-
-      
-
-      //return bar color
-      d3.select(this)
-              .style("fill", "darkred");
-
-      TOOLTIP.style("opacity", 0); 
-    };
-
-    // Add event listeners
-    FRAME2.selectAll(".bar")
-          .on("mouseover", handleMouseover) 
-          .on("mousemove", handleMousemove)
-          .on("mouseleave", handleMouseleave);    
-
-
-  });
